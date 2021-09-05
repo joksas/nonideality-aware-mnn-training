39c9c32e93d29d684c683b9564401cd2cb4f576a
Dovydas <dovydas@yoshke.org>
2021-09-05 21:45:09 +0100

    add tests for `crossbar.nonlinear_IV.compute_currents()`

---
+++ tests/crossbar/test_nonlinear_IV.py
@@ -0,0 +1,116 @@
+"""
+Tests of functions of crossbar.nonlinear_IV
+"""
+# pylint: disable=missing-function-docstring
+import tensorflow as tf
+import pytest
+from crossbar import nonlinear_IV
+from . import utils
+
+
+# I feel it is appropriate to use multiplication for expected tensors because
+# it is not the underlying operation that we are testing. Writing it out
+# reveals the logic behind the calculations that *should* take place - Dovydas
+test_compute_currents_testdata = [
+        (
+            {
+                "n_avg": tf.constant(2.0),
+                "n_std": tf.constant(0.0),
+                "V_ref": tf.constant(0.5),
+                "G": tf.constant([
+                    [1.0, 2.0, 3.0, 4.0],
+                    [5.0, 6.0, 7.0, 8.0],
+                    ]),
+                "V": tf.constant([
+                    [1.0, 0.0],
+                    ]),
+                },
+            tf.constant([
+                [
+                    [1.0*1.0, 1.0*2.0, 1.0*3.0, 1.0*4.0],
+                    [0.0*5.0, 0.0*6.0, 0.0*7.0, 0.0*8.0],
+                    ],
+                ])
+            ),
+        (
+            {
+                "n_avg": tf.constant(2.0),
+                "n_std": tf.constant(0.0),
+                "V_ref": tf.constant(0.5),
+                "G": tf.constant([
+                    [1.0, 2.0, 3.0, 4.0],
+                    [5.0, 6.0, 7.0, 8.0],
+                    ]),
+                "V": tf.constant([
+                    [1.0, 0.5],
+                    ]),
+                },
+            tf.constant([
+                [
+                    [1.0*1.0, 1.0*2.0, 1.0*3.0, 1.0*4.0],
+                    [0.5*5.0, 0.5*6.0, 0.5*7.0, 0.5*8.0],
+                    ],
+                ])
+            ),
+        (
+            {
+                "n_avg": tf.constant(4.0),
+                "n_std": tf.constant(0.0),
+                "V_ref": tf.constant(0.5),
+                "G": tf.constant([
+                    [1.0, 2.0, 3.0, 4.0],
+                    [5.0, 6.0, 7.0, 8.0],
+                    [9.0, 10.0, 11.0, 12.0],
+                    ]),
+                "V": tf.constant([
+                    [0.0, 0.5, 1.0],
+                    ]),
+                },
+            tf.constant([
+                [
+                    [0.0*1.0, 0.0*2.0, 0.0*3.0, 0.0*4.0],
+                    # Baseline because V_ref = 0.5
+                    [0.5*5.0, 0.5*6.0, 0.5*7.0, 0.5*8.0],
+                    # Multiplying by additional factor of 4 because 1/0.5 = 2
+                    # and n_avg = 4
+                    [0.5*9.0*4, 0.5*10.0*4, 0.5*11.0*4, 0.5*12.0*4],
+                    ],
+                ])
+            ),
+        (
+                {
+                    "n_avg": tf.constant(3.0),
+                    "n_std": tf.constant(0.0),
+                    "V_ref": tf.constant(0.2),
+                    "G": tf.constant([
+                        [1.0, 2.0, 3.0, 4.0],
+                        [5.0, 6.0, 7.0, 8.0],
+                        ]),
+                    "V": tf.constant([
+                        [0.0, 0.2],
+                        [0.1, 0.4],
+                        ]),
+                    },
+                tf.constant([
+                    [
+                        [0.0*1.0, 0.0*2.0, 0.0*3.0, 0.0*4.0],
+                        # Baseline because V_ref = 0.2
+                        [0.2*5.0, 0.2*6.0, 0.2*7.0, 0.2*8.0],
+                        ],
+                    [
+                        # Dividing by additional factor of 3 because 0.1/0.2 =
+                        # 1/2 and n_avg = 3
+                        [0.2*1.0/3.0, 0.2*2.0/3.0, 0.2*3.0/3.0, 0.2*4.0/3.0],
+                        # Multiplying by additional factor of 3 because 0.4/0.2
+                        # = 2 and n_avg = 3
+                        [0.2*5.0*3.0, 0.2*6.0*3.0, 0.2*7.0*3.0, 0.2*8.0*3.0],
+                        ],
+                    ])
+                ),
+        ]
+
+
+@pytest.mark.parametrize("args,expected", test_compute_currents_testdata)
+def test_compute_currents(args, expected):
+    I = nonlinear_IV.compute_currents(**args)
+    utils.assert_tf_approx(I, expected)
---
+++ tests/crossbar/utils.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+
+def assert_tf_approx(a, b):
+    tf.debugging.assert_near(a, b, rtol=1.0e-6, atol=1.0e-6)
+
