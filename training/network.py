6f8ab4c7ef1bbcb9c62a082d323a9b0db5174dbb
Dovydas <dovydas@yoshke.org>
2021-09-09 09:21:02 +0100

    only allow MNIST or CIFAR-10

--- training/network.py
+++ training/network.py
@@ -7,14 +7,14 @@ from tensorflow import keras
 from tensorflow.keras.datasets import cifar10, mnist
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-sys.path.insert(0, '..')
+sys.path.insert(0, "..")
 
 
 def model_compile_kwargs():
     return {
             "loss": "sparse_categorical_crossentropy",
             "optimizer": keras.optimizers.SGD(lr=0.01),
-            "metrics": ['accuracy'],
+            "metrics": ["accuracy"],
             }
 
 
@@ -27,33 +27,39 @@ def train(iterator):
     cback=keras.callbacks.ModelCheckpoint(iterator.weights_path(), monitor="val_accuracy", save_best_only=True)
 
     if iterator.use_generator:
-        if iterator.dataset=="CIFAR-10" or iterator.dataset=="binarynet":
+        if iterator.dataset=="CIFAR-10":
             horizontal_flip=True
-        if iterator.dataset=="SVHN" or iterator.dataset=="binarynet-svhn":
-            horizontal_flip=False
 
         datagen = ImageDataGenerator(
                 width_shift_range=0.15,  # randomly shift images horizontally (fraction of total width)
                 height_shift_range=0.15,  # randomly shift images vertically (fraction of total height)
                 horizontal_flip=horizontal_flip)  # randomly flip images
-        if keras.__version__[0]=='2':
+        if keras.__version__[0]=="2":
             history=model.fit_generator(
-                    datagen.flow(iterator.x_train, iterator.y_train, batch_size=iterator.training.batch_size),
+                    datagen.flow(
+                        iterator.x_train, iterator.y_train,
+                        batch_size=iterator.training.batch_size),
                     steps_per_epoch=iterator.x_train.shape[0]/iterator.training.batch_size,
-                    nb_epoch=iterator.training.num_epochs, validation_split=0.1,
-                    verbose=2, callbacks=[cback])
-        if keras.__version__[0]=='1':
+                    nb_epoch=iterator.training.num_epochs,
+                    validation_split=0.1,
+                    verbose=2,
+                    callbacks=[cback])
+        if keras.__version__[0]=="1":
             history=model.fit_generator(
-                    datagen.flow(iterator.x_train, iterator.y_train, batch_size=iterator.training.batch_size),
-                    samples_per_epoch=iterator.x_train.shape[0], nb_epoch=iterator.training.num_epochs,
-                    verbose=2, validation_split=0.1, callbacks=[cback])
+                    datagen.flow(iterator.x_train, iterator.y_train,
+                        batch_size=iterator.training.batch_size),
+                    samples_per_epoch=iterator.x_train.shape[0],
+                    nb_epoch=iterator.training.num_epochs,
+                    verbose=2,
+                    validation_split=0.1,
+                    callbacks=[cback])
     else:
-        if keras.__version__[0]=='2':
+        if keras.__version__[0]=="2":
             history=model.fit(iterator.x_train, iterator.y_train, batch_size=iterator.training.batch_size,validation_split=0.1, verbose=2, epochs=iterator.training.num_epochs,callbacks=[cback])
-        if keras.__version__[0]=='1':
+        if keras.__version__[0]=="1":
             history=model.fit(iterator.x_train, iterator.y_train, batch_size=iterator.training.batch_size,validation_split=0.1, verbose=2, nb_epoch=iterator.training.num_epochs,callbacks=[cback])
 
-    dic={'hard': history.history}
+    dic={"hard": history.history}
     with open(iterator.history_path(), "wb") as handle:
         pickle.dump(dic, handle)
 
--- training/utils.py
+++ training/utils.py
@@ -24,7 +24,7 @@ def compute_device_power(V, I_ind):
     """
     # $P = VI$ for individual devices. All devices in the same word
     # line of the crossbar (row of G) are applied with the same voltage.
-    P_ind = tf.einsum('ij,ijk->ijk', V, I_ind)
+    P_ind = tf.einsum("ij,ijk->ijk", V, I_ind)
 
     return P_ind
 
@@ -55,48 +55,26 @@ def compute_avg_crossbar_power(V, I_ind):
     return P_avg
 
 
-def load_svhn(path_to_dataset):
-    train = sio.loadmat(path_to_dataset+'/train.mat')
-    test = sio.loadmat(path_to_dataset+'/test.mat')
-    extra = sio.loadmat(path_to_dataset+'/extra.mat')
-    x_train = np.transpose(train['X'], [3, 0, 1, 2])
-    y_train = train['y']-1
-
-    x_test = np.transpose(test['X'], [3, 0, 1, 2])
-    y_test = test['y']-1
-
-    x_extra = np.transpose(extra['X'], [3, 0, 1, 2])
-    y_extra=extra['y']-1
-
-    x_train = np.concatenate((x_train, x_extra), axis=0)
-    y_train = np.concatenate((y_train, y_extra), axis=0)
-
-    return (x_train, y_train), (x_test, y_test)
-
 def get_examples(dataset):
     if dataset == "MNIST":
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        # convert class vectors to binary class matrices
-        x_train = x_train.reshape(-1,784)
-        x_test = x_test.reshape(-1,784)
+        x_train = x_train.reshape(-1, 784)
+        x_test = x_test.reshape(-1, 784)
         use_generator = False
-    elif dataset == "CIFAR-10" or dataset == "binarynet":
-        use_generator = True
+    elif dataset == "CIFAR-10":
         (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    elif dataset == "SVHN" or dataset == "binarynet-svhn":
         use_generator = True
-        (x_train, y_train), (x_test, y_test) = load_svhn('./svhn_data')
     else:
-        raise("dataset should be one of the following: [MNIST, CIFAR-10, SVHN, binarynet, binarynet-svhn].")
+        raise "Dataset should be one of the following: [MNIST, CIFAR-10]."
 
     x_train = x_train.astype(np.float32)
     x_test = x_test.astype(np.float32)
     x_train /= 255
     x_test /= 255
 
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
+    print("x_train shape:", x_train.shape)
+    print(x_train.shape[0], "train samples")
+    print(x_test.shape[0], "test samples")
 
     return x_train, y_train, x_test, y_test, use_generator
 
