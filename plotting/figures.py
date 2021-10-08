522814a44c0fe521332eab963eea4a37d9081b8d
Dovydas Joksas <dovydas@yoshke.org>
2021-10-08 17:17:59 +0100

    add histogram and boxplot figures

--- plotting/figures.py
+++ plotting/figures.py
@@ -1,11 +1,15 @@
 from training.iterator import Iterator, Training, IVNonlinearity, Inference, StuckAtGMin, D2DLognormal
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import simulations
 from . import utils
+from training import architecture
+import crossbar
+import tensorflow as tf
 
 AXIS_LABEL_FONT_SIZE = 12
-LEGEND_FONT_SIZE = 10
+LEGEND_FONT_SIZE = 8
 TICKS_FONT_SIZE = 8
 SUBPLOT_LABEL_SIZE = 12
 LINEWIDTH=0.75
@@ -76,6 +80,48 @@ def iv_nonlinearity_error_curves():
 
     plt.savefig("plotting/error-curves.pdf", bbox_inches="tight")
 
+
+def iv_nonlinearity_boxplots():
+    fig, axes = plt.subplots(figsize=(9/2.54, 7.0/2.54))
+    fig.tight_layout()
+    iterators = simulations.iv_nonlinearity.get_iterators()
+    iterators.insert(3, iterators[0])
+    indices = [0, 0, 0, 1, 0, 0]
+    errors = [100*iterator.err()[idx].flatten() for idx, iterator in zip(indices, iterators)]
+    powers = [iterator.avg_power()[idx].flatten() for idx, iterator in zip(indices, iterators)]
+    colors = [utils.color_dict()[key] for key in ["vermilion", "blue", "bluish-green"]]
+    labels = ["Standard", "Nonideality-aware", "Nonideality-aware (regularised)"]
+
+    boxplots = []
+
+    for idx, (error, power) in enumerate(zip(errors, powers)):
+        x_pos = np.mean(power)
+        w = 0.2
+        color = colors[idx%3]
+        boxplot = plt.boxplot(error, positions=[x_pos],
+                widths=[10**(np.log10(x_pos)+w/2.)-10**(np.log10(x_pos)-w/2.)], sym=color)
+        # plt.setp(boxplot["fliers"], marker="x", markersize=4)
+        # for element in ["boxes", "whiskers", "fliers", "means", "medians", "caps"]:
+        #     plt.setp(boxplot[element], color=color)
+        plt.setp(boxplot["fliers"], marker="x", markersize=1, markeredgewidth=0.5)
+        for element in ["boxes", "whiskers", "fliers", "means", "medians", "caps"]:
+            plt.setp(boxplot[element], color=color, linewidth=0.5)
+
+        boxplots.append(boxplot)
+
+    axes.legend([boxplot["boxes"][0] for boxplot in boxplots[:3]], [label for label in labels],
+            fontsize=LEGEND_FONT_SIZE, frameon=False)
+
+    plt.xticks(fontsize=TICKS_FONT_SIZE)
+    plt.yticks(fontsize=TICKS_FONT_SIZE)
+    plt.xlabel("Ohmic power consumption (W)", fontsize=AXIS_LABEL_FONT_SIZE)
+    plt.ylabel("Test error (%)", fontsize=AXIS_LABEL_FONT_SIZE)
+    axes.set_xscale("log")
+    axes.set_yscale("log")
+
+    plt.savefig("plotting/iv-nonlinearity-boxplots.pdf", bbox_inches="tight")
+
+
 def cnn_results():
     fig, axes = plt.subplots(1, 3, sharey=True, figsize=(18/2.54, 5.0/2.54))
     fig.tight_layout()
@@ -97,8 +143,7 @@ def cnn_results():
 
     # Box plots.
     axis = axes[2]
-    accuracies = [iterator.acc()[0].flatten() for iterator in iterators]
-    errors = [100*(1-accuracy) for accuracy in accuracies]
+    errors = [100*iterator.err()[0].flatten() for iterator in iterators]
     for idx, (error, color) in enumerate(zip(errors, [colors["vermilion"], colors["blue"]])):
         bplot = axis.boxplot(error, positions=[idx])
         plt.setp(bplot["fliers"], marker="x", markersize=1, markeredgewidth=0.2)
@@ -118,3 +163,46 @@ def cnn_results():
     plt.figlegend(["Training", "Validation"], ncol=2, bbox_to_anchor=(0, 0, 0.55, 1.15), frameon=False)
 
     plt.savefig("plotting/cnn-boxplots.pdf", bbox_inches="tight")
+
+def d2d_conductance_histograms():
+    fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(18/2.54, 9/2.54))
+
+    iterators = simulations.d2d_asymmetry.get_iterators()
+    colors = [utils.color_dict()[key] for key in ["vermilion", "blue"]]
+
+    for idx, (axis, iterator, color) in enumerate(zip(axes, iterators, colors)):
+        iterator.is_training = True
+        model = architecture.get_model(iterator, custom_weights_path=iterator.weights_path())
+        weights = model.layers[0].combined_weights()
+        G, _ = crossbar.map.w_params_to_G(weights, iterator.training.G_min, iterator.training.G_max)
+        G = 1000*G
+        axis.hist(G.numpy().flatten(), bins=100, color=color)
+        utils.add_subfigure_label(fig, axis, idx, SUBPLOT_LABEL_SIZE)
+        axis.set_xlabel("Conductance (mS)", fontsize=AXIS_LABEL_FONT_SIZE)
+
+    axes[0].set_ylabel("Count (#)", fontsize=AXIS_LABEL_FONT_SIZE)
+
+    plt.savefig("plotting/d2d-G-histogram.pdf", bbox_inches="tight")
+
+
+def d2d_boxplots():
+    fig, axes = plt.subplots(figsize=(9/2.54, 7.0/2.54))
+    fig.tight_layout()
+    iterators = simulations.d2d_asymmetry.get_iterators()
+    errors = [100*iterator.err()[0].flatten() for iterator in iterators]
+    colors = [utils.color_dict()[key] for key in ["vermilion", "blue"]]
+
+    boxplots = []
+
+    for idx, (error, color) in enumerate(zip(errors, colors)):
+        bplot = plt.boxplot(error, positions=[idx], sym=color)
+        plt.setp(bplot["fliers"], marker="x", markersize=2, markeredgewidth=0.5)
+        for element in ["boxes", "whiskers", "fliers", "means", "medians", "caps"]:
+            plt.setp(bplot[element], color=color, linewidth=0.5)
+        plt.xticks([0, 1], ["Uniform", "Asymmetric"], fontsize=TICKS_FONT_SIZE)
+    
+    axes.set_yscale("log")
+    plt.xlabel("D2D variability", fontsize=AXIS_LABEL_FONT_SIZE)
+    plt.ylabel("Eror (%)", fontsize=AXIS_LABEL_FONT_SIZE)
+
+    plt.savefig("plotting/d2d-boxplots.pdf", bbox_inches="tight")
