fix calculation of average power and allow to get MNN layer weights

training/architecture.py
@@ -119,13 +119,7 @@ class MemristorDense(layers.Layer):
                     **kwargs
                     )
 
-    def call(self, x, mask=None):
-        if not self.iterator.training.is_aware() and self.iterator.is_training:
-            return tf.tensordot(x, self.w, axes=1) + self.b
-
-        ones = tf.ones([tf.shape(x)[0], 1])
-        inputs = tf.concat([x, ones], 1)
-
+    def combined_weights(self):
         if self.iterator.training.is_aware():
             b_pos = tf.expand_dims(self.b_pos, axis=0)
             b_neg = tf.expand_dims(self.b_neg, axis=0)
@@ -144,7 +138,16 @@ class MemristorDense(layers.Layer):
             bias = tf.expand_dims(self.b, axis=0)
             combined_weights = tf.concat([self.w, bias], 0)
 
-        self.out = self.memristive_outputs(inputs, combined_weights)
+        return combined_weights
+
+    def call(self, x, mask=None):
+        if not self.iterator.training.is_aware() and self.iterator.is_training:
+            return tf.tensordot(x, self.w, axes=1) + self.b
+
+        ones = tf.ones([tf.shape(x)[0], 1])
+        inputs = tf.concat([x, ones], 1)
+
+        self.out = self.memristive_outputs(inputs, self.combined_weights())
 
         return self.out
 

training/iterator.py
@@ -264,8 +264,8 @@ class Iterator(Dataset):
 
     def avg_power(self):
         average_powers = []
-        for inference_idx in range(self.inference_idx):
-            self.inference_idx = self.inference_idx
+        for inference_idx in range(len(self.inferences)):
+            self.inference_idx = inference_idx
             inference = self.inferences[self.inference_idx]
             average_power = np.zeros((self.training.num_repeats, inference.num_repeats))
 
@@ -285,6 +285,8 @@ class Iterator(Dataset):
 
             self.training.repeat_idx = 0
             average_powers.append(average_power)
+
+        self.inference_idx = None
 
         return average_powers
 