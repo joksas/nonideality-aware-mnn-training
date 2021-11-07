aecdaf6853b7233b8f7d1aee2726031f3d14ef2a
Dovydas Joksas <dovydas@yoshke.org>
2021-11-07 17:31:54 +0000

    simplify `kde()` and make it more stable
    Using `tf.float32` (instead of `tf.float64`) seems to solve numerical
    stability issues when small bandwidth is passed.

--- crossbar/faulty_devices.py
+++ crossbar/faulty_devices.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.ops.numpy_ops import np_config
 from tensorflow_probability import distributions as tfd
 
 
@@ -42,27 +43,30 @@ def kde(means: list[float], bandwidth: float) -> tfd.Distribution:
     Returns:
         KDE distribution.
     """
-    distributions = []
     weights = []
+    distr_means = []
     for mean in means:
-        limits = {
-            "low": 0.0,
-            "high": np.inf,
-        }
-        truncated = tfd.TruncatedNormal(loc=mean, scale=bandwidth, **limits)
-        distributions.append(truncated)
+        distr_means.append(mean)
         prob_neg = tfd.Normal(loc=mean, scale=bandwidth).cdf(0.0)
         if prob_neg > 1e-8:  # Only include reflection if numerically stable.
-            reflected_truncated = tfd.TruncatedNormal(loc=-mean, scale=bandwidth, **limits)
-            distributions.append(reflected_truncated)
+            distr_means.append(-mean)
             prob_pos = 1.0 - prob_neg
             weights.extend([prob_pos, prob_neg])
         else:
             weights.append(1.0)
 
-    kde_distribution = tfd.Mixture(
-        cat=tfd.Categorical(probs=weights),
-        components=distributions,
+    np_config.enable_numpy_behavior()
+    distr_means_32 = tf.constant(distr_means)
+    bandwidth_32 = tf.constant(bandwidth)
+
+    kde_distribution = tfd.MixtureSameFamily(
+        tfd.Categorical(probs=weights),
+        tfd.TruncatedNormal(
+            loc=distr_means_32.astype(tf.float32),
+            scale=bandwidth_32.astype(tf.float32),
+            low=0.0,
+            high=np.inf,
+        ),
     )
 
     return kde_distribution
