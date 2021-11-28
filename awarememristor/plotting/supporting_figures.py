e5793026a80721f21c49173d4c09d3f52c39c44b
Dovydas Joksas <dovydas@yoshke.org>
2021-11-28 23:27:40 +0000

    move all I-V curves plot to supporting figures file

--- awarememristor/plotting/supporting_figures.py
+++ awarememristor/plotting/supporting_figures.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from awarememristor import simulations
@@ -42,3 +43,46 @@ def iv_nonlinearity_training_curves(metric="error"):
     )
 
     utils.save_fig(fig, f"iv-nonlinearity-training-{metric}", is_supporting=True)
+
+
+def iv_curves_all():
+    fig, axes = utils.fig_init(1, 0.8, fig_shape=(1, 1))
+
+    data = simulations.data.load_SiO_x()
+    voltages, currents = simulations.data.all_SiO_x_curves(data)
+
+    N = 1000
+    palette = plt.cm.inferno(np.linspace(0, 1, N))
+
+    min_voltage, max_voltage = 0.0, 0.5
+
+    for idx in range(voltages.shape[0]):
+        voltage_curve = voltages[idx, :]
+        current_curve = currents[idx, :]
+        n = simulations.data.nonlinearity_parameter(current_curve)
+        palette_idx = int(np.floor(N * (n - 2) / 2))
+        axes.plot(
+            voltage_curve,
+            current_curve,
+            color=palette[palette_idx],
+            linewidth=utils.Config.LINEWIDTH,
+        )
+
+    axes.set_xlim([min_voltage, max_voltage])
+    axes.set_xlabel(utils.axis_label("voltage"))
+    axes.yaxis.get_offset_text().set_fontsize(utils.Config.TICKS_FONT_SIZE)
+
+    sm = plt.cm.ScalarMappable(cmap="inferno", norm=plt.Normalize(vmin=2, vmax=4))
+    cbar = fig.colorbar(sm, ax=axes)
+    cbar.set_label(
+        label=utils.axis_label("nonlinearity-parameter"),
+        fontsize=utils.Config.AXIS_LABEL_FONT_SIZE,
+        rotation=-90,
+        va="bottom",
+    )
+    cbar.ax.tick_params(axis="both", which="both", labelsize=utils.Config.TICKS_FONT_SIZE)
+
+    axes.set_ylabel(utils.axis_label("current"))
+    axes.set_yscale("log")
+
+    utils.save_fig(fig, "SiO_x-IV-curves-all", is_supporting=True)
